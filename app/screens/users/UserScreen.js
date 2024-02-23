c9d4fbfc45a5dec941779156bae006e35816a8f7
Artem Puzik <artem.puzik.it@gmail.com>
2024-02-23 13:22:17 +0300

    run to xcode

--- app/screens/users/UserScreen.js
+++ app/screens/users/UserScreen.js
@@ -24,7 +24,7 @@ const UserScreen = ({navigation}) => {
         setIsLoading(true)
         await dispatch(setCurrent(user)).then(() => setTimeout(() => {
             setIsLoading(false)
-            navigation.navigate('Objects')
+            navigation.navigate('Home')
             }, 1000))
     }, [])
 
